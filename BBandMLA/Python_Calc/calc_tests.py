724e0088867e6e9e28e9bd7400a323a1248c2295
Ludwig Lind <ludwig.lind@physik.tu-darmstadt.de>
2024-10-10 13:34:08 +0200

    include 2D plot

--- BBandMLA/Python_Calc/calc_tests.py
+++ BBandMLA/Python_Calc/calc_tests.py
@@ -11,41 +11,60 @@ import math as m
 import matplotlib.pyplot as plt
 import scipy.special as ss
 
+# Defining pathes for importing own modules
+import sys
+sys.path.insert(1, "C:/Users/Ludwig/OneDrive/Dokumente/GitKraken/ATOMICS-python-modules/basic-modules/")
+sys.path.insert(1, "//brain43/groups/Atomics/ATOMICS-python-modules/basic-modules")
+
+import graphical_analysis as ga
+
 E0 = 1       
 
 n = 400
 k_max = 10
 
 Z = 0
-k = 1
-p = 2
-rho0= np.linspace(0.8,1.1,n)
+p_list = np.linspace(0,2.8,200)
+rho0_list = np.linspace(0.8,1.1,n)
 """
 B0 and B2; case Z = 0
 """
-x3 = -rho0**2
+
 # first value for central and second for nearest neighbors
-B0 = [0,0]
+
 # B2 = [0,0]
-B0[0] = 2*ss.hyp1f1(1, 1/2, x3)
-# B2[0] = 3*rho0**ss.hyp1f1(2, 3/2, x3)
-for k in range(k_max):
-    B0[1] += 2*ss.hyp1f1(k+1, 1/2, x3)*(-p**2)**k/m.factorial(k)
+I0 = np.zeros((len(rho0_list),len(p_list)))
+Icross = np.zeros((len(rho0_list),len(p_list)))
+
+for i in range(len(rho0_list)):
+    rho0 = rho0_list[i]
+    x3 = -rho0**2               
+    B00 = 2*ss.hyp1f1(1, 1/2, x3)
+    for j in range(len(p_list)):
+        p = p_list[j]
+        B01 = 0
+        for k in range(k_max):
+            B01 += 2*ss.hyp1f1(k+1, 1/2, x3)*(-p**2)**k/m.factorial(k)
+        I0[i,j] = B00**2
+        Icross[i,j] = (B00+4*B01)**2
     # B2[1] += 4*rho0*ss.hyp1f1(k+2, 3/2, x3)*(-p**2)**k/m.factorial(k)
 
-I0 = B0[0]**2
-Icross = (B0[0]+4*B0[1])**2
+# I0 = B00**2
+# Icross = (B00+4*B01)**2
 
 """
 Plot
 """
-fig, ax = plt.subplots()
-plt.grid(True)
-ax.set_xlabel(r'$\rho_0$')
-ax.set_ylabel('value')
 
-ax.plot(rho0, Icross, 'b-', label = r'cross')
-ax.plot(rho0, I0, 'g-', label = r'single')
-# ax.plot(rho0_list, N, 'r-.', label = r'Kummer imag')
-plt.legend(loc = 'best', prop = {'size':15})
-plt.show()
+ga.reel_2D(p_list, rho0_list, I0, xlabel='pitch', ylabel=r'$\rho0$')
+ga.reel_2D(p_list, rho0_list, Icross, xlabel='pitch', ylabel=r'$\rho0$')
+# fig, ax = plt.subplots()
+# plt.grid(True)
+# ax.set_xlabel(r'pitch to waist')
+# ax.set_ylabel('value')
+
+# ax.plot(p, Icross, 'b-', label = r'cross')
+# # ax.plot(p, I0*25, 'g-', label = r'single')
+# # ax.plot(rho0_list, N, 'r-.', label = r'Kummer imag')
+# plt.legend(loc = 'best', prop = {'size':15})
+# plt.show()
