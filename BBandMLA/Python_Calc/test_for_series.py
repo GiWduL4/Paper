5fe2289f92b7a25fa6ac6ff934716ecacd64d25f
Ludwig Lind <ludwig.lind@physik.tu-darmstadt.de>
2024-10-11 18:59:40 +0200

    texts on test case

--- BBandMLA/Python_Calc/test_for_series.py
+++ BBandMLA/Python_Calc/test_for_series.py
@@ -5,16 +5,23 @@ Created on Fri Oct 11 18:51:20 2024
 @author: Ludwig P. Lind
 """
 
-from mpmath import nsum, inf
+from mpmath import nsum, inf, fac
 
 # Euler's transformation for faster convergence
 def alternating_series(k):
-    return (-1)**k / (k + 1)
+    return (-2)**k / fac(k)
 
-# sum_result = nsum(alternating_series, [0, inf])
-# print(sum_result)
+sum_result = nsum(alternating_series, [0, inf])
+print(sum_result)
+
 A = 0
-for k in range(100):
+n = 500000
+step = 0
+for k in range(n):
     A += alternating_series(k)
+    progress = (k+1)/n
+    if progress >= step:
+        print('Progress: ' + str(round(progress*100,3)) + ' %')
+        step += 0.1
 
 print(A)
