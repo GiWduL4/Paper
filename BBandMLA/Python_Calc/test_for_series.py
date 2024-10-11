1e1246e0e1a2f516ea6d310c43540e015d3c9397
Ludwig Lind <ludwig.lind@physik.tu-darmstadt.de>
2024-10-12 01:14:59 +0200

    testcase for mpmath with hyp1f1

--- BBandMLA/Python_Calc/test_for_series.py
+++ BBandMLA/Python_Calc/test_for_series.py
@@ -5,17 +5,24 @@ Created on Fri Oct 11 18:51:20 2024
 @author: Ludwig P. Lind
 """
 
-from mpmath import nsum, inf, fac
+from mpmath import nsum, inf, fac, hyp1f1
+import scipy.special as ss
 
 # Euler's transformation for faster convergence
+
+r = 6.5
+r2 = r**2
+rho0 = 1.5
+
+x3 = - rho0**2
 def alternating_series(k):
-    return (-2)**k / fac(k)
+    return hyp1f1(k+1, 1/2, x3) /fac(k) * (-r2)**k
 
 sum_result = nsum(alternating_series, [0, inf])
 print(sum_result)
 
 A = 0
-n = 500000
+n = 200
 step = 0
 for k in range(n):
     A += alternating_series(k)
