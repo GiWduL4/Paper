cca2cbfc34f4d693b270f4d996104854a1cac399
Ludwig Lind <ludwig.lind@physik.tu-darmstadt.de>
2024-10-28 14:40:04 +0100

    chatgpt optimization

--- BBandMLA/Python_Calc/config_for_assemble.py
+++ BBandMLA/Python_Calc/config_for_assemble.py
@@ -6,112 +6,57 @@ Created on Mon Oct 28 13:55:30 2024
 """
 
 def configuration(E_full, E, x_s, y_s, size, j, setup):
-    """
-    Parameters
-    ----------
-    E_full : np.array of shape (2,n_x_full,n_y_full)
-        full array of E-field
-    E : np.array of shape (2, size, size)
-        E-field of single beam for given parameters
-    x_s : integer
-        start index for adding E onto E_full in central position
-    y_s : integer
-        same as x_s but in y direction
-    size : integer
-        size of single E-filed
-    j : integer
-        index that matches shift p
-    setup : string
-        string that gives type of setup. options are:
-            'cross', 'single', 'X', '3x3', '1x3', '3x1', '5x5'
+    x_e, y_e = x_s + size, y_s + size
 
-    Returns
-    -------
-    E_full: with added E fields of the different beams
+    # Define helper functions for each setup type
+    def single_beam():
+        E_full[:, x_s:x_e, y_s:y_e] += E
 
-    """
-    x_e = x_s + size
-    y_e = y_s + size
-    
-    if setup == 'single':
-        E_full[:,x_s:x_s+size, y_s:y_s+size] += E
-    
-    elif setup == 'cross':
-        E_full[:,x_s:x_e, y_s:y_s+size] += E
-        
-        E_full[:,x_s+j:x_e+j, y_s:y_e] += E
-        E_full[:,x_s-j:x_e-j, y_s:y_e] += E
-        E_full[:,x_s:x_e, y_s+j:y_e+j] += E
-        E_full[:,x_s:x_e, y_s-j:y_e-j] += E
-    
-    elif setup == 'X':
-        E_full[:,x_s:x_s+size, y_s:y_s+size] += E
-        
-        E_full[:,x_s+j:x_e+j, y_s+j:y_e+j] += E
-        E_full[:,x_s+j:x_e+j, y_s-j:y_e-j] += E
-        E_full[:,x_s-j:x_e-j, y_s+j:y_e+j] += E
-        E_full[:,x_s-j:x_e-j, y_s-j:y_e-j] += E
-        
-    elif setup == '3x3':
-        E_full[:,x_s:x_e, y_s:y_s+size] += E
-        
-        E_full[:,x_s+j:x_e+j, y_s:y_e] += E
-        E_full[:,x_s-j:x_e-j, y_s:y_e] += E
-        E_full[:,x_s:x_e, y_s+j:y_e+j] += E
-        E_full[:,x_s:x_e, y_s-j:y_e-j] += E
-        
-        E_full[:,x_s+j:x_e+j, y_s+j:y_e+j] += E
-        E_full[:,x_s+j:x_e+j, y_s-j:y_e-j] += E
-        E_full[:,x_s-j:x_e-j, y_s+j:y_e+j] += E
-        E_full[:,x_s-j:x_e-j, y_s-j:y_e-j] += E
-        
-    elif setup == '1x3':
-        E_full[:,x_s:x_e, y_s:y_s+size] += E
-        
-        E_full[:,x_s:x_e, y_s+j:y_e+j] += E
-        E_full[:,x_s:x_e, y_s-j:y_e-j] += E
-        
-    elif setup == '3x1':
-        E_full[:,x_s:x_e, y_s:y_s+size] += E
-        
-        E_full[:,x_s+j:x_e+j, y_s:y_e] += E
-        E_full[:,x_s-j:x_e-j, y_s:y_e] += E
-        
-    elif setup == '5x5':
-        E_full[:,x_s:x_e, y_s:y_s+size] += E
-        
-        E_full[:,x_s+j:x_e+j, y_s:y_e] += E
-        E_full[:,x_s-j:x_e-j, y_s:y_e] += E
-        E_full[:,x_s:x_e, y_s+j:y_e+j] += E
-        E_full[:,x_s:x_e, y_s-j:y_e-j] += E
-        
-        E_full[:,x_s+j:x_e+j, y_s+j:y_e+j] += E
-        E_full[:,x_s+j:x_e+j, y_s-j:y_e-j] += E
-        E_full[:,x_s-j:x_e-j, y_s+j:y_e+j] += E
-        E_full[:,x_s-j:x_e-j, y_s-j:y_e-j] += E   
-        
-        E_full[:,x_s+2*j:x_e+2*j, y_s:y_e] += E
-        E_full[:,x_s-2*j:x_e-2*j, y_s:y_e] += E
-        E_full[:,x_s:x_e, y_s+2*j:y_e+2*j] += E
-        E_full[:,x_s:x_e, y_s-2*j:y_e-2*j] += E
-        
-        E_full[:,x_s+2*j:x_e+2*j, y_s+2*j:y_e+2*j] += E
-        E_full[:,x_s+2*j:x_e+2*j, y_s-2*j:y_e-2*j] += E
-        E_full[:,x_s-2*j:x_e-2*j, y_s+2*j:y_e+2*j] += E
-        E_full[:,x_s-2*j:x_e-2*j, y_s-2*j:y_e-2*j] += E  
-        
-        E_full[:,x_s+1*j:x_e+1*j, y_s+2*j:y_e+2*j] += E
-        E_full[:,x_s+1*j:x_e+1*j, y_s-2*j:y_e-2*j] += E
-        E_full[:,x_s-1*j:x_e-1*j, y_s+2*j:y_e+2*j] += E
-        E_full[:,x_s-1*j:x_e-1*j, y_s-2*j:y_e-2*j] += E 
-        
-        E_full[:,x_s+2*j:x_e+2*j, y_s+1*j:y_e+1*j] += E
-        E_full[:,x_s+2*j:x_e+2*j, y_s-1*j:y_e-1*j] += E
-        E_full[:,x_s-2*j:x_e-2*j, y_s+1*j:y_e+1*j] += E
-        E_full[:,x_s-2*j:x_e-2*j, y_s-1*j:y_e-1*j] += E  
+    def cross_beams():
+        positions = [(0, 0), (j, 0), (-j, 0), (0, j), (0, -j)]
+        for dx, dy in positions:
+            E_full[:, x_s+dx:x_e+dx, y_s+dy:y_e+dy] += E
 
+    def x_beams():
+        positions = [(0, 0), (j, j), (j, -j), (-j, j), (-j, -j)]
+        for dx, dy in positions:
+            E_full[:, x_s+dx:x_e+dx, y_s+dy:y_e+dy] += E
+
+    def grid_3x3():
+        shifts = [0, j, -j]
+        for dx in shifts:
+            for dy in shifts:
+                E_full[:, x_s+dx:x_e+dx, y_s+dy:y_e+dy] += E
+
+    def grid_5x5():
+        shifts = [0, j, -j, 2*j, -2*j]
+        for dx in shifts:
+            for dy in shifts:
+                E_full[:, x_s+dx:x_e+dx, y_s+dy:y_e+dy] += E
+
+    def grid_8x8():
+        shifts = [0, j, -j, 2*j, -2*j, 3*j, -3*j, 4*j, -4*j]
+        for dx in shifts:
+            for dy in shifts:
+                if abs(dx) <= 4*j and abs(dy) <= 4*j:  # Ensuring the range for 8x8 grid
+                    E_full[:, x_s+dx:x_e+dx, y_s+dy:y_e+dy] += E
+
+    # Map setup names to their corresponding functions
+    setups = {
+        'single': single_beam,
+        'cross': cross_beams,
+        'X': x_beams,
+        '3x3': grid_3x3,
+        '5x5': grid_5x5,
+        '8x8': grid_8x8,
+    }
+
+    # Execute the chosen setup
+    if setup in setups:
+        setups[setup]()
     else:
-        print('Invalid Setup Input')
-    
-    return(E_full)
+        raise ValueError("Invalid Setup Input")
+
+    return E_full
+
     
